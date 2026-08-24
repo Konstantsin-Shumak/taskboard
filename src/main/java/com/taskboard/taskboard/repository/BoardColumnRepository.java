17f2993c63a8424e6f63d600ae8949cd5dc012a2
Konstantsin <konstantsin.shumak@gmail.com>
2026-08-25 00:01:02 +0200

    feat: add GET /api/boards/{id}/columns nested endpoint

--- src/main/java/com/taskboard/taskboard/repository/BoardColumnRepository.java
+++ src/main/java/com/taskboard/taskboard/repository/BoardColumnRepository.java
@@ -3,6 +3,10 @@ package com.taskboard.taskboard.repository;
 import com.taskboard.taskboard.domain.BoardColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardColumnRepository extends JpaRepository<BoardColumn, Long> {
-    long countByBoardId(Long boardId);
+    long countByBoard_Id(Long boardId);
+
+    List<BoardColumn> findByBoard_Id(Long boardId);
 }
